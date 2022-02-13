8b17efc3134524a58f954a6f87a649f644058f8d
vilim <mail@vil.im>
2022-02-13 19:31:35 +0100

    basic zebrafish tracking formats

--- src/spec/create_extension_spec.py
+++ src/spec/create_extension_spec.py
@@ -2,6 +2,7 @@
 import os.path
 
 from pynwb.spec import NWBNamespaceBuilder, export_spec, NWBGroupSpec, NWBAttributeSpec
+
 # TODO: import other spec classes as needed
 # from pynwb.spec import NWBDatasetSpec, NWBLinkSpec, NWBDtypeSpec, NWBRefSpec
 
@@ -12,43 +13,62 @@ def main():
         doc="""NWB extensions for storing (larval) zebrafish expreiment related data""",
         name="""ndx-zebrafish""",
         version="""0.1.0""",
-        author=list(map(str.strip, """Ruben Portugues, Martin Haesemeyer, Luigi Petrucco, Vilim Stih""".split(','))),
-        contact=list(map(str.strip, """ruben.portugues@tum.de""".split(',')))
+        author=list(
+            map(
+                str.strip,
+                """Ruben Portugues, Martin Haesemeyer, Luigi Petrucco, Vilim Stih""".split(
+                    ","
+                ),
+            )
+        ),
+        contact=list(map(str.strip, """ruben.portugues@tum.de""".split(","))),
     )
 
-    # TODO: specify the neurodata_types that are used by the extension as well
-    # as in which namespace they are found.
-    # this is similar to specifying the Python modules that need to be imported
-    # to use your new data types.
-    # all types included or used by the types specified here will also be
-    # included.
-    ns_builder.include_type('ElectricalSeries', namespace='core')
+    ns_builder.include_type("ZebrafishBehaviorSeries", namespace="core")
 
     # TODO: define your new data types
     # see https://pynwb.readthedocs.io/en/latest/extensions.html#extending-nwb
     # for more information
-    tetrode_series = NWBGroupSpec(
-        neurodata_type_def='TetrodeSeries',
-        neurodata_type_inc='ElectricalSeries',
-        doc=('An extension of ElectricalSeries to include the tetrode ID for '
-             'each time series.'),
+    FishGroup = NWBGroupSpec(
+        neurodata_type_def="ZebrafishBehaviorSeries",
+        neurodata_type_inc="NWBContainer",
+        doc="An extension of the NWB group to include all behavioral data from a zebrafish traking experiemnt",
         attributes=[
-            NWBAttributeSpec(
-                name='trode_id',
-                doc='The tetrode ID.',
-                dtype='int32'
-            )
+            NWBAttributeSpec(name="fish_id", doc="The fish ID.", dtype="int32")
         ],
     )
+    FishGroup.add_dataset(
+        default_name="orientation",
+        neurodata_type_def="Orientation",
+        neurodata_type_inc="SpatialSeries",
+        quantity="?",
+        doc="The direction the fish head points in a left coordinate system"
+        " (angle increases clockwise, zero points right",
+    )
+    FishGroup.add_dataset(
+        default_name="tail_shape",
+        neurodata_type_def="TailShape",
+        neurodata_type_inc="SpatialSeries",
+        quantity="?",
+        doc="The shape of the tail recorded as relative angles to first tail segment",
+    )
+    FishGroup.add_dataset(
+        default_name="position",
+        neurodata_type_def="Coordinates",
+        neurodata_type_inc="SpatialSeries",
+        quantity="?",
+        doc="The x,y position of the swim bladder in camera coordinates",
+    )
 
-    # TODO: add all of your new data types to this list
-    new_data_types = [tetrode_series]
+    new_data_types = [FishGroup]
 
     # export the spec to yaml files in the spec folder
-    output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'spec'))
+    output_dir = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..", "..", "spec")
+    )
     export_spec(ns_builder, new_data_types, output_dir)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # usage: python create_extension_spec.py
     main()
